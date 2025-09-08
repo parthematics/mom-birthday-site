268d224c85e91820dd6660f56b7c9ec3733b4cd9
parth <parth.chopra@berkeley.edu>
2025-09-08 00:44:28 -0700

    favicon

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if (!$authenticated) {
 
     <head>
         <title>Family Access Required</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎂</text></svg>">
         <style>
             body {
                 font-family: "Times New Roman", Times, serif;
@@ -108,6 +109,7 @@ $last_updated = date('F Y');
 
 <head>
     <title>Happy Birthday <?php echo $mom_name; ?></title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎂</text></svg>">
     <style>
         body {
             font-family: "Times New Roman", Times, serif;
